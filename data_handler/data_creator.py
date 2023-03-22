598e2183e25182b217614d26c9585fd1bc76a9fa
yashartayyarazad99 <yashartayyarazad99@gmail.com>
2023-03-22 23:26:08 +0330

    fix bugs

--- data_handler/data_creator.py
+++ data_handler/data_creator.py
@@ -107,37 +107,53 @@ class DataCreator():
         shutil.rmtree(self.dst, ignore_errors=True)
         os.makedirs(self.dst, exist_ok=True)
 
-        shutil.copy(src=os.path.join(self.src, 'ordibehesht_10K.tar.gz'), dst=self.dst)
+        shutil.copy(src=os.path.join(self.src, 'roshan_internship_full_dataset_cleaned.zip'), dst=self.dst)
         shutil.copy(src=os.path.join(self.src, 'ordibehesht.csv.tar.gz'), dst=self.dst)
 
-        data_file = tarfile.open(os.path.join(self.dst, 'ordibehesht_10K.tar.gz'))
-        data_file.extractall(self.dst)
-        data_file.close()
+        shutil.unpack_archive(os.path.join(self.dst, 'roshan_internship_full_dataset_cleaned.zip'), self.dst)
 
         metadata_file = tarfile.open(os.path.join(self.dst, 'ordibehesht.csv.tar.gz'))
         metadata_file.extractall(self.dst)
         metadata_file.close()
 
-        metadata = pd.read_csv('/content/ordibehesht.csv')
+        metadata = pd.read_csv(os.path.join(self.dst, 'ordibehesht.csv'))
         metadata.dropna(inplace=True)
 
-        image_names = os.listdir(os.path.join(self.dst, 'ordibehesht_images_10000'))
+        image_names = os.listdir(os.path.join(self.dst, 'roshan_internship_full_dataset_cleaned'))
+        image_names = [image_name.split('.')[0] for image_name in image_names]
 
         for image_dir, labels_list in metadata.itertuples(index=False, name=None):
             labels_list = labels_list.replace('"', '').replace(']', '').replace('[', '').split(',')
             if 'true' in labels_list:
                 label = labels_list[labels_list.index('true') - 1]
+            else:
+                continue
 
-            image_name = image_dir.split('/')[-1]
+            image_name = image_dir.split('/')[-1].split('.')[0]
             if image_name in image_names:
                 os.makedirs(os.path.join(self.dst, 'dataset', label), exist_ok=True)
-                shutil.move(src=os.path.join(self.dst, 'ordibehesht_images_10000', image_name), dst=os.path.join(self.dst, 'dataset', label))
+                shutil.move(src=os.path.join(self.dst, 'roshan_internship_full_dataset_cleaned', image_name + '.jpg'), dst=os.path.join(self.dst, 'dataset', label))
                 image_names.remove(image_name)
 
             if len(image_names) == 0:
                 break
 
+        raw_classes = os.listdir(os.path.join(self.dst, 'dataset'))
+
+        le = LabelEncoder()
+        le.fit(raw_classes)
+        classes = list(le.classes_)
+        encoded_classes = list(le.transform(classes))
+
+        encoded_classes_dict = {}
+        
+        for cls, encoded_cls in zip(classes, encoded_classes):
+            encoded_classes_dict[cls] = encoded_cls
+
         print('data folder created successfully.\n')
+
+        self.classes = classes
+        self.encoded_classes_dict = encoded_classes_dict
 
     def partitioning(self, partitioning_base_folder:str = '../dataset', val_ratio:float = 0.15, test_ratio:float = 0.15, seed:float = None):
 
@@ -152,12 +168,10 @@ class DataCreator():
         if not train_classes == val_classes == test_classes:
             raise FileNotFoundError('data is not completely ready!\ncheck that you run create_data_folder method correctly.')
 
-        classes = os.listdir(os.path.join(self.dst, 'dataset'))
-
         partition = {'train':[], 'val':[], 'test':[]}
         labels = {}
 
-        for cls in classes:
+        for cls in self.classes:
 
             train_files = np.array(glob(os.path.join(partitioning_base_folder, 'train', cls, '*')))
             val_files = np.array(glob(os.path.join(partitioning_base_folder, 'val', cls, '*')))
@@ -180,14 +194,17 @@ class DataCreator():
 
         print('Classes and train/val/test counts:\n')
         
-        for cls in classes:
+        for cls in self.classes:
 
             n_train = len(os.listdir(os.path.join(partitioning_base_folder, 'train', cls)))
             n_val = len(os.listdir(os.path.join(partitioning_base_folder, 'val', cls)))
             n_test = len(os.listdir(os.path.join(partitioning_base_folder, 'test', cls)))
+            n_all = n_train + n_val + n_test
 
-            print(f'{cls} >>> train: {n_train} | val: {n_val} | test: {n_test}')
+            print(f'Total number of images from {cls} class: {n_all}')
+
+            print(f'{cls} >>> train: {n_train} | val: {n_val} | test: {n_test}\n')
 
         print('\n')
 
-        return partition, labels
+        return partition, labels, self.encoded_classes_dict
